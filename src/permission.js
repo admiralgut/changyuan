459e0254783ec0d791638f67a65be310ea3ddd29
admiralgut <1067632892@qqcom>
2024-08-06 19:19:42 +0800

    first

---
+++ src/permission.js
@@ -0,0 +1,34 @@
+import { LoadingBar } from 'view-design'
+import router from './router'
+import store from './store'
+import createRoutes from '@/utils/createRoutes'
+import { getDocumentTitle, resetTokenAndClearUser } from './utils'
+
+// 是否有菜单数据
+let hasMenus = false
+router.beforeEach(async (to, from, next) => {
+    document.title = getDocumentTitle(to.meta.title)
+    LoadingBar.start()
+    if (hasMenus) {
+        next()
+    } else {
+        try {
+            // 这里可以用 await 配合请求后台数据来生成路由
+            // const data = await axios.get('xxx')
+            // const routes = createRoutes(data)
+            const routes = createRoutes(store.state.menuItems)
+            // 动态添加路由
+            router.addRoutes(routes)
+            hasMenus = true
+            next({ path: to.path || '/' })
+        } catch (error) {
+            resetTokenAndClearUser()
+            // 直接重定向到根路径或者预设路径
+            next('/')
+        }
+    }
+})
+
+router.afterEach(() => {
+    LoadingBar.finish()
+})
